054b21d0135f8a0f51eec540ca30956f080df3a2
Jorge <jorgerivera12293@gmail.com>
2025-03-02 14:38:53 -0500

    Update JWT

--- src/main/java/com/base/api/api_base_security/services/auth/JwtService.java
+++ src/main/java/com/base/api/api_base_security/services/auth/JwtService.java
@@ -1,17 +1,16 @@
 package com.base.api.api_base_security.services.auth;
 
-import java.security.Key;
 import java.util.Date;
 import java.util.Map;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 @Service 
@@ -26,18 +25,21 @@ public class JwtService {
     String generateToken(UserDetails user,Map<String,Object> extraClaims){
         Date issuedAt = new Date(System.currentTimeMillis());
         Date expiration = new Date((EXPIRATION_IN_MINUTES*6*1000) + issuedAt.getTime());
-        String jwt = Jwts.builder()
-            .setClaims(extraClaims)
-            .setSubject(user.getUsername())
-            .setIssuedAt(issuedAt)
-            .setExpiration(expiration)
-            .setHeaderParam(Header.TYPE,Header.JWT_TYPE)
-            .signWith(generatedKey(),SignatureAlgorithm.HS256)
+
+        String jwt = Jwts.builder() 
+            .header()
+                .type("JWT")
+                .and()
+            .subject(user.getUsername())
+            .issuedAt(issuedAt)
+            .expiration(expiration)
+            .claims(extraClaims)
+            .signWith(generatedKey(), Jwts.SIG.HS256)
             .compact();
         return jwt;
     }
 
-    private Key generatedKey() {
+    private SecretKey generatedKey() {
         byte[] passwordDecodeString = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(passwordDecodeString);
     }
@@ -47,9 +49,8 @@ public class JwtService {
     }
         
     private Claims extractAllClaims(String jwt) {
-        return Jwts.parser()
-            .setSigningKey(generatedKey()).build()
-            .parseClaimsJws(jwt).getBody();
+        return Jwts.parser().verifyWith(generatedKey()).build()
+            .parseSignedClaims(jwt).getPayload();
     }
     
 }
