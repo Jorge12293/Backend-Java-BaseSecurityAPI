47fb741305afa0e054359bcd719441d50ea36fad
Jorge <jorgerivera12293@gmail.com>
2025-03-02 09:48:44 -0500

    Implements Security Filter Cahin

---
+++ src/main/java/com/base/api/api_base_security/config/security/HttpSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.base.api.api_base_security.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class HttpSecurityConfig {
+
+    final private AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        SecurityFilterChain filterChain = http
+            .csrf(csrfConfig->csrfConfig.disable())
+            .sessionManagement(ssMagConfig->ssMagConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authenticationProvider)
+            .authorizeHttpRequests(authReqConfig->{
+                authReqConfig.requestMatchers(HttpMethod.POST,"/customers").permitAll();
+                authReqConfig.requestMatchers(HttpMethod.POST,"/auth/**").permitAll();
+            })
+            .build();
+        return filterChain;    
+    }
+}
--- src/main/java/com/base/api/api_base_security/exception/InvalidPasswordException.java
+++ src/main/java/com/base/api/api_base_security/exception/InvalidPasswordException.java
@@ -1,15 +1,16 @@
 package com.base.api.api_base_security.exception;
 
-public class InvalidPasswordException extends RuntimeException{
-    
-    public InvalidPasswordException() {
-    }
- 
-    public InvalidPasswordException(String message) {
-       super(message);
-    }
- 
-    public InvalidPasswordException(String message, Throwable cause) {
-       super(message, cause);
-    }
+public class InvalidPasswordException extends RuntimeException {
+
+   public InvalidPasswordException() {
+   }
+
+   public InvalidPasswordException(String message) {
+      super(message);
+   }
+
+   public InvalidPasswordException(String message, Throwable cause) {
+      super(message, cause);
+   }
+   
 }
