967d77a35cdc761d20a6f4c70ae9feb534b9262b
Jorge <jorgerivera12293@gmail.com>
2025-03-02 21:38:24 -0500

    Implementar Expression Regular

--- src/main/java/com/base/api/api_base_security/config/security/HttpSecurityConfig.java
+++ src/main/java/com/base/api/api_base_security/config/security/HttpSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configurers.AuthorizeH
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 import com.base.api.api_base_security.config.filter.JwtAuthenticationFilter;
 import com.base.api.api_base_security.persistence.util.Role;
@@ -40,7 +41,8 @@ public class HttpSecurityConfig {
         authReqConfig.requestMatchers(HttpMethod.GET,"/products")
             .hasAnyRole(Role.ADMINISTRATOR.name(),Role.ASSISTANT_ADMINISTRATOR.name());
 
-        authReqConfig.requestMatchers(HttpMethod.GET,"/products/{productId}")
+        authReqConfig.requestMatchers(RegexRequestMatcher.regexMatcher(HttpMethod.GET,"/products/[0-9]*")) // Expression Regular
+        //authReqConfig.requestMatchers(HttpMethod.GET,"/products/{productId}")
             .hasAnyRole(Role.ADMINISTRATOR.name(),Role.ASSISTANT_ADMINISTRATOR.name());
         
         authReqConfig.requestMatchers(HttpMethod.POST,"/products")
